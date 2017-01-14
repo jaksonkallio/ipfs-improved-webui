66f1a4eda208a861b21c7afeefed5d10bfc86b38
jaksonkallio <jakson@protonmail.com>
2017-01-14 17:20:02 -0600

    For each pin, build function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
 function listPinned(){
   $.get("http://127.0.0.1:5001/api/v0/pin/ls", function(data){
-    console.log(data);
+    $.each(data.Keys, function(key, value){
+      buildListItem(key, data['Keys'][key]['type']);
+    });
   });
+}
+
+function buildListItem(key, pin_type){
+  console.log("Key: "+key+" - Pin Type: "+pin_type);
 }
 
 $(document).ready(function(){
