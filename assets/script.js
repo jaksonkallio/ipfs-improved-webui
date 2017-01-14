aebe9c62cec83c832d3e6a605ab7f65d6eab2dd7
jaksonkallio <jakson@protonmail.com>
2017-01-14 17:52:03 -0600

    Check if known file type

--- assets/script.js
+++ assets/script.js
@@ -23,14 +23,18 @@ function buildListItem(file_key, pin_type){
     url: httpFileUrl(file_key),
     success: function(data, textstatus, request){
       var content_type_raw = request.getResponseHeader('Content-Type');
+      content_type_raw = content_type_raw.split('/');
       
-      console.log(content_type_raw);
+      if(avail_content_types['image'].indexOf(content_type_raw[1]) > -1){
+        updateItemPreview(file_key, 'image');
+      
+      }
     }
   }); 
 }
 
 function updateItemPreview(file_key, content_type){
-
+  console.log(file_key+" is a(n) "+content_type);
 }
 
 function httpFileUrl(file_key){
